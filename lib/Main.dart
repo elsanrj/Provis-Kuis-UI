fcce6ea801bdcb0f1728d59d6112a30ac916e938
Ina <149356749+dinaagustinaa@users.noreply.github.com>
2025-03-14 07:28:35 +0700

    Main.dart

---
+++ lib/Main.dart
@@ -0,0 +1,101 @@
+import 'Package:flutter/material.dart';
+import 'HomePage.dart';
+import 'DetailPage.dart';
+import 'ChatBooking.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kuiz 1: UI ~Elsa x Ina',
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MainPage(),
+        '/home': (context) => HomePage(),
+        '/detail': (context) => DetailPage(),
+        '/chat': (context) => ChatBooking(),
+      },
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Pemrograman Visual & Piranti Bergerak',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Quiz 1: UI',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'No kelompok praktikum: 58',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/home');
+                },
+                child: Text('Halaman Depan', selectionColor: Color.fromARGB(255, 24, 151, 102)),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/detail');
+                },
+                child: Text('Rincian Item'),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/chat');
+                },
+                child: Text('Chat'),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {}, child: Text('Wishlist')),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {}, child: Text('Keranjang & Checkout')),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {}, child: Text('Pembelian Paket')),
+              SizedBox(height: 20),
+              Text('Transaksi', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {}, child: Text('Monitor Pesanan')),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {}, child: Text('Pengembalian')),
+              SizedBox(height: 20),
+              Text('User', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {}, child: Text('Pembeli')),
+              SizedBox(height: 10),
+              ElevatedButton(onPressed: () {}, child: Text('Penjual')),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
